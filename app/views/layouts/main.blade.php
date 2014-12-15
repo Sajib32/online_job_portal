0717c2e8b7a158b9b66aef0e76a5132af689e54e
Sajib32 <Sajibnstu@gmail.com>
2014-12-15 18:32:27 +0600

    Testing multi-progress bar jquery plugin.

--- app/views/layouts/main.blade.php
+++ app/views/layouts/main.blade.php
@@ -8,13 +8,14 @@
 
     <!-- Latest compiled and minified CSS -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
-
 <!-- Optional theme -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap-theme.min.css">
-
 <!-- Latest compiled and minified JavaScript -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
-  </head>
+
+<link rel="stylesheet" href="{{ URL::asset('css/style.css')}}">
+
+</head>
   <body>
     <h1>Hello, world!</h1>
 
@@ -26,5 +27,12 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="js/bootstrap.min.js"></script>
+
+    <!-- jQuery -->
+<script src="http://thecodeplayer.com/uploads/js/jquery-1.9.1.min.js" type="text/javascript"></script>
+<!-- jQuery easing plugin -->
+<script src="http://thecodeplayer.com/uploads/js/jquery.easing.min.js" type="text/javascript"></script> 
+<script src="{{ URL::asset('js/script.js')}}"></script>
+
   </body>
 </html>
