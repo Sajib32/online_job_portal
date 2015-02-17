e853ebca1053010e2448da534a7f904792b611f5
Sajib32 <Sajibnstu@gmail.com>
2015-02-17 16:06:24 +0600

    Bootstrap steps wizard testing..

--- app/views/layouts/test.blade.php
+++ app/views/layouts/test.blade.php
@@ -6,70 +6,182 @@
         <link rel="stylesheet" href="{{ URL::asset('css/normalize.css') }}">
         <link rel="stylesheet" href="{{ URL::asset('css/main.css') }}">
         <link rel="stylesheet" href="{{ URL::asset('css/jquery.steps.css') }}">
+        <link rel="stylesheet" href="{{ URL::asset('css/bootstrap.min.css') }}">
+        <link rel="stylesheet" href="{{ URL::asset('css/formValidation.css') }}">
         <script src="{{ URL::asset('js/modernizr-2.6.2.min.js') }}"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
         <script src="{{ URL::asset('js/jquery.cookie-1.3.1.js') }}"></script>
         <script src="{{ URL::asset('js/jquery.steps.js') }}"></script>
         <script src="{{ URL::asset('js/jquery.validate.js') }}"></script>
         <script src="{{ URL::asset('js/jquery.validate.min.js') }}"></script>
+        <script src="{{ URL::asset('js/bootstrap.min.js') }}"></script>
         <script src="{{ URL::asset('js/formValidation.js') }}"></script>
         <script src="{{ URL::asset('js/formValidation.min.js') }}"></script>
+         <!-- Include Bootstrap Wizard -->
+<script src="{{ URL::asset('js/jquery.bootstrap.wizard.js') }}"></script>
+<script src="{{ URL::asset('js/jquery.bootstrap.wizard.min.js') }}"></script>
+<style>
+body{ 
+    margin-top:40px; 
+}
 
-       
-    </head>
-     <body>  
-<script type="text/javascript">
-$(function() {
-    $("#form1").steps({
-        headerTag: "h2",
-        bodyTag: "fieldset",
-        transitionEffect: "slideLeft",
-        onStepChanging: function(event, currentIndex, newIndex) {
-             $("#form1").validate().settings.ignore = ":disabled,:hidden";
-             return $("#form1").valid();
-        },
-        onFinishing: function(event, currentIndex) {
-            $("#form1").validate().settings.ignore = ":disabled";
-            return $("#form1").valid();
-        },
-        onFinished: function(event, currentIndex) {
-            alert("Submitted!");
+.stepwizard-step p {
+    margin-top: 10px;
+}
+
+.stepwizard-row {
+    display: table-row;
+}
+
+.stepwizard {
+    display: table;
+    width: 100%;
+    position: relative;
+}
+
+.stepwizard-step button[disabled] {
+    opacity: 1 !important;
+    filter: alpha(opacity=100) !important;
+}
+
+.stepwizard-row:before {
+    top: 14px;
+    bottom: 0;
+    position: absolute;
+    content: " ";
+    width: 100%;
+    height: 1px;
+    background-color: #ccc;
+    z-order: 0;
+
+}
+
+.stepwizard-step {
+    display: table-cell;
+    text-align: center;
+    position: relative;
+}
+
+.btn-circle {
+  width: 30px;
+  height: 30px;
+  text-align: center;
+  padding: 6px 0;
+  font-size: 12px;
+  line-height: 1.428571429;
+  border-radius: 15px;
+}
+</style>       
+</head>
+        
+
+     <body>
+
+
+<div class="container">
+<div class="stepwizard">
+    <div class="stepwizard-row setup-panel">
+        <div class="stepwizard-step">
+            <a href="#step-1" type="button" class="btn btn-primary btn-circle">1</a>
+            <p>Step 1</p>
+        </div>
+        <div class="stepwizard-step">
+            <a href="#step-2" type="button" class="btn btn-default btn-circle" disabled="disabled">2</a>
+            <p>Step 2</p>
+        </div>
+        <div class="stepwizard-step">
+            <a href="#step-3" type="button" class="btn btn-default btn-circle" disabled="disabled">3</a>
+            <p>Step 3</p>
+        </div>
+    </div>
+</div>
+<form role="form">
+    <div class="row setup-content" id="step-1">
+        <div class="col-xs-12">
+            <div class="col-md-12">
+                <h3> Step 1</h3>
+                <div class="form-group">
+                    <label class="control-label">First Name</label>
+                    <input  maxlength="100" type="text" required="required" class="form-control" placeholder="Enter First Name"  />
+                </div>
+                <div class="form-group">
+                    <label class="control-label">Last Name</label>
+                    <input maxlength="100" type="text" required="required" class="form-control" placeholder="Enter Last Name" />
+                </div>
+                <button class="btn btn-primary nextBtn btn-lg pull-right" type="button" >Next</button>
+            </div>
+        </div>
+    </div>
+    <div class="row setup-content" id="step-2">
+        <div class="col-xs-12">
+            <div class="col-md-12">
+                <h3> Step 2</h3>
+                <div class="form-group">
+                    <label class="control-label">Company Name</label>
+                    <input maxlength="200" type="text" required="required" class="form-control" placeholder="Enter Company Name" />
+                </div>
+                <div class="form-group">
+                    <label class="control-label">Company Address</label>
+                    <input maxlength="200" type="text" required="required" class="form-control" placeholder="Enter Company Address"  />
+                </div>
+                <button class="btn btn-primary nextBtn btn-lg pull-right" type="button" >Next</button>
+            </div>
+        </div>
+    </div>
+    <div class="row setup-content" id="step-3">
+        <div class="col-xs-12">
+            <div class="col-md-12">
+                <h3> Step 3</h3>
+                <button class="btn btn-success btn-lg pull-right" type="submit">Finish!</button>
+            </div>
+        </div>
+    </div>
+</form>
+</div>
+<script>
+    $(document).ready(function () {
+
+    var navListItems = $('div.setup-panel div a'),
+            allWells = $('.setup-content'),
+            allNextBtn = $('.nextBtn');
+
+    allWells.hide();
+
+    navListItems.click(function (e) {
+        e.preventDefault();
+        var $target = $($(this).attr('href')),
+                $item = $(this);
+
+        if (!$item.hasClass('disabled')) {
+            navListItems.removeClass('btn-primary').addClass('btn-default');
+            $item.addClass('btn-primary');
+            allWells.hide();
+            $target.show();
+            $target.find('input:eq(0)').focus();
         }
-    }).validate({
-        errorPlacement: function(error, element) {
-            element.before(error);
-        },
-        rules: {
-            confirm: {
-                equalTo: "#password"
+    });
+
+    allNextBtn.click(function(){
+        var curStep = $(this).closest(".setup-content"),
+            curStepBtn = curStep.attr("id"),
+            nextStepWizard = $('div.setup-panel div a[href="#' + curStepBtn + '"]').parent().next().children("a"),
+            curInputs = curStep.find("input[type='text'],input[type='url']"),
+            isValid = true;
+
+        $(".form-group").removeClass("has-error");
+        for(var i=0; i<curInputs.length; i++){
+            if (!curInputs[i].validity.valid){
+                isValid = false;
+                $(curInputs[i]).closest(".form-group").addClass("has-error");
             }
         }
-    });            
 
+        if (isValid)
+            nextStepWizard.removeAttr('disabled').trigger('click');
+    });
+
+    $('div.setup-panel div a.btn-primary').trigger('click');
 });
 </script>
-<form id="form1"  runat="server">          
-            <h2>First Step</h2>
-            <fieldset>
-                  <legend> First Step Info </legend>  
-                    <label for="userName">User name *</label>
-                    <input id="userName" name="userName" type="text" class="required">
-                    <label for="password">Password *</label>
-                    <input id="password" name="password" type="text" class="required">
-                    <label for="confirm">Confirm Password *</label>
-                    <input id="confirm" name="confirm" type="text" class="required">
-                    <p>(*) Mandatory</p>
-
-            </fieldset>
-
-            <h2>Second Step</h2>
-            <fieldset>
-                <legend>Second Step Info</legend>   
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ut nulla nunc. Maecenas arcu sem, hendrerit a tempor quis, 
-                    sagittis accumsan tellus. In hac habitasse platea dictumst. Donec a semper dui. Nunc eget quam libero. Nam at felis metus. 
-                    Nam tellus dolor, tristique ac tempus nec, iaculis quis nisi.
-
-            </fieldset>                                             
-</form>
     </body>
 </html>
