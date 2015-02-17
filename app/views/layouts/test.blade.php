3b0ce8dfa27dc078c22c9a04f81db727bcd9f19c
Sajib32 <Sajibnstu@gmail.com>
2015-02-17 17:45:39 +0600

    I can add manually and validate in this template of bootstrap wizard.

--- app/views/layouts/test.blade.php
+++ app/views/layouts/test.blade.php
@@ -3,185 +3,110 @@
     <head>
         <title>Demo</title>
         <meta charset="utf-8">
-        <link rel="stylesheet" href="{{ URL::asset('css/normalize.css') }}">
-        <link rel="stylesheet" href="{{ URL::asset('css/main.css') }}">
-        <link rel="stylesheet" href="{{ URL::asset('css/jquery.steps.css') }}">
+        <link rel="stylesheet" href="{{ URL::asset('css/bootstrap.css') }}">
         <link rel="stylesheet" href="{{ URL::asset('css/bootstrap.min.css') }}">
-        <link rel="stylesheet" href="{{ URL::asset('css/formValidation.css') }}">
-        <script src="{{ URL::asset('js/modernizr-2.6.2.min.js') }}"></script>
+        <link rel="stylesheet" href="{{ URL::asset('css/prettify.css') }}">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-        <script src="{{ URL::asset('js/jquery.cookie-1.3.1.js') }}"></script>
-        <script src="{{ URL::asset('js/jquery.steps.js') }}"></script>
-        <script src="{{ URL::asset('js/jquery.validate.js') }}"></script>
-        <script src="{{ URL::asset('js/jquery.validate.min.js') }}"></script>
         <script src="{{ URL::asset('js/bootstrap.min.js') }}"></script>
-        <script src="{{ URL::asset('js/formValidation.js') }}"></script>
-        <script src="{{ URL::asset('js/formValidation.min.js') }}"></script>
-         <!-- Include Bootstrap Wizard -->
-<script src="{{ URL::asset('js/jquery.bootstrap.wizard.js') }}"></script>
-<script src="{{ URL::asset('js/jquery.bootstrap.wizard.min.js') }}"></script>
-<style>
-body{ 
-    margin-top:40px; 
-}
-
-.stepwizard-step p {
-    margin-top: 10px;
-}
-
-.stepwizard-row {
-    display: table-row;
-}
-
-.stepwizard {
-    display: table;
-    width: 100%;
-    position: relative;
-}
-
-.stepwizard-step button[disabled] {
-    opacity: 1 !important;
-    filter: alpha(opacity=100) !important;
-}
-
-.stepwizard-row:before {
-    top: 14px;
-    bottom: 0;
-    position: absolute;
-    content: " ";
-    width: 100%;
-    height: 1px;
-    background-color: #ccc;
-    z-order: 0;
-
-}
-
-.stepwizard-step {
-    display: table-cell;
-    text-align: center;
-    position: relative;
-}
-
-.btn-circle {
-  width: 30px;
-  height: 30px;
-  text-align: center;
-  padding: 6px 0;
-  font-size: 12px;
-  line-height: 1.428571429;
-  border-radius: 15px;
-}
-</style>       
+        <script src="{{ URL::asset('js/bootstrap.js') }}"></script>
+        <script src="{{ URL::asset('js/prettify.js') }}"></script>
+        <script src="{{ URL::asset('js/jquery.bootstrap.wizard.js') }}"></script>
+        <script src="{{ URL::asset('js/jquery.validate.min.js') }}"></script>
 </head>
-        
+<body>
+    <div class='container'>
 
-     <body>
+            <section id="wizard">
+              <div class="page-header">
+                <h1>Basic Pills Wizard</h1>
+              </div>
+                <form id="commentForm" method="get" action="" class="form-horizontal">
+                <div id="rootwizard">
+                    <ul>
+                        <li><a href="#tab1" data-toggle="tab">First</a></li>
+                        <li><a href="#tab2" data-toggle="tab">Second</a></li>
+                        <li><a href="#tab3" data-toggle="tab">Third</a></li>
+                        <li><a href="#tab3" data-toggle="tab">Third</a></li>
+                    </ul>
+                    <div class="tab-content">
+                        <div class="tab-pane" id="tab1">
+                            <div class="control-group">
+                                <label class="control-label" for="email">Email</label>
+                                <div class="controls">
+                                  <input type="text" id="emailfield" name="emailfield" class="required email">
+                                </div>
+                              </div>
 
-
-<div class="container">
-<div class="stepwizard">
-    <div class="stepwizard-row setup-panel">
-        <div class="stepwizard-step">
-            <a href="#step-1" type="button" class="btn btn-primary btn-circle">1</a>
-            <p>Step 1</p>
-        </div>
-        <div class="stepwizard-step">
-            <a href="#step-2" type="button" class="btn btn-default btn-circle" disabled="disabled">2</a>
-            <p>Step 2</p>
-        </div>
-        <div class="stepwizard-step">
-            <a href="#step-3" type="button" class="btn btn-default btn-circle" disabled="disabled">3</a>
-            <p>Step 3</p>
-        </div>
-    </div>
-</div>
-<form role="form">
-    <div class="row setup-content" id="step-1">
-        <div class="col-xs-12">
-            <div class="col-md-12">
-                <h3> Step 1</h3>
-                <div class="form-group">
-                    <label class="control-label">First Name</label>
-                    <input  maxlength="100" type="text" required="required" class="form-control" placeholder="Enter First Name"  />
+                              <div class="control-group">
+                                <label class="control-label" for="name">Name</label>
+                                <div class="controls">
+                                  <input type="text" id="namefield" name="namefield" class="required">
+                                </div>
+                              </div>
+                              <div class="control-group">
+                                <label class="control-label" for="name">Fathers Name</label>
+                                <div class="controls">
+                                  <input type="text" id="fatherfield" name="fatherfield" class="required">
+                                </div>
+                              </div>
+                        </div>
+                        <div class="tab-pane" id="tab2">
+                          <div class="control-group">
+                                <label class="control-label" for="url">URL</label>
+                                <div class="controls">
+                                  <input type="text" id="urlfield" name="urlfield" class="required url">
+                                </div>
+                              </div>
+                        </div>
+                        <div class="tab-pane" id="tab3">
+                            3
+                        </div>
+                        <ul class="pager wizard">
+                            <li class="previous first" style="display:none;"><a href="#">First</a></li>
+                            <li class="previous"><a href="#">Previous</a></li>
+                            <li class="next last" style="display:none;"><a href="#">Last</a></li>
+                            <li class="next"><a href="#">Next</a></li>
+                        </ul>
+                    </div>
                 </div>
-                <div class="form-group">
-                    <label class="control-label">Last Name</label>
-                    <input maxlength="100" type="text" required="required" class="form-control" placeholder="Enter Last Name" />
-                </div>
-                <button class="btn btn-primary nextBtn btn-lg pull-right" type="button" >Next</button>
-            </div>
-        </div>
-    </div>
-    <div class="row setup-content" id="step-2">
-        <div class="col-xs-12">
-            <div class="col-md-12">
-                <h3> Step 2</h3>
-                <div class="form-group">
-                    <label class="control-label">Company Name</label>
-                    <input maxlength="200" type="text" required="required" class="form-control" placeholder="Enter Company Name" />
-                </div>
-                <div class="form-group">
-                    <label class="control-label">Company Address</label>
-                    <input maxlength="200" type="text" required="required" class="form-control" placeholder="Enter Company Address"  />
-                </div>
-                <button class="btn btn-primary nextBtn btn-lg pull-right" type="button" >Next</button>
-            </div>
-        </div>
-    </div>
-    <div class="row setup-content" id="step-3">
-        <div class="col-xs-12">
-            <div class="col-md-12">
-                <h3> Step 3</h3>
-                <button class="btn btn-success btn-lg pull-right" type="submit">Finish!</button>
-            </div>
-        </div>
-    </div>
-</form>
-</div>
-<script>
-    $(document).ready(function () {
-
-    var navListItems = $('div.setup-panel div a'),
-            allWells = $('.setup-content'),
-            allNextBtn = $('.nextBtn');
-
-    allWells.hide();
-
-    navListItems.click(function (e) {
-        e.preventDefault();
-        var $target = $($(this).attr('href')),
-                $item = $(this);
-
-        if (!$item.hasClass('disabled')) {
-            navListItems.removeClass('btn-primary').addClass('btn-default');
-            $item.addClass('btn-primary');
-            allWells.hide();
-            $target.show();
-            $target.find('input:eq(0)').focus();
-        }
-    });
-
-    allNextBtn.click(function(){
-        var curStep = $(this).closest(".setup-content"),
-            curStepBtn = curStep.attr("id"),
-            nextStepWizard = $('div.setup-panel div a[href="#' + curStepBtn + '"]').parent().next().children("a"),
-            curInputs = curStep.find("input[type='text'],input[type='url']"),
-            isValid = true;
-
-        $(".form-group").removeClass("has-error");
-        for(var i=0; i<curInputs.length; i++){
-            if (!curInputs[i].validity.valid){
-                isValid = false;
-                $(curInputs[i]).closest(".form-group").addClass("has-error");
+                </form>
+    <script>
+    $(document).ready(function() {
+        var $validator = $("#commentForm").validate({
+          rules: {
+            emailfield: {
+              required: true,
+              email: true,
+              minlength: 3
+            },
+            namefield: {
+              required: true,
+              minlength: 3
+            },
+            fatherfield:{
+              required: true,
+              minlength: 3
+            },
+            urlfield: {
+              required: true,
+              minlength: 3,
+              url: true
             }
-        }
+          }
+        });
 
-        if (isValid)
-            nextStepWizard.removeAttr('disabled').trigger('click');
+        $('#rootwizard').bootstrapWizard({
+            'tabClass': 'nav nav-pills',
+            'onNext': function(tab, navigation, index) {
+                var $valid = $("#commentForm").valid();
+                if(!$valid) {
+                    $validator.focusInvalid();
+                    return false;
+                }
+            }
+        });
+        window.prettyPrint && prettyPrint()
     });
-
-    $('div.setup-panel div a.btn-primary').trigger('click');
-});
-</script>
-    </body>
+    </script>
+</body>
 </html>
